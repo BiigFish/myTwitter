9588e59a20be42cce6b0a667981d5ff240b0902f
Evan <evchan29@gmail.com>
2023-08-08 18:54:57 -0400

    24

--- posts.js
+++ posts.js
@@ -10,13 +10,13 @@ async function fetchData() {
             listItem.innerHTML = `
                 <li class="bg-gray-200 rounded-lg p-2">
                   <h2 class="font-bold text-sm text-red-400 italic">${item.date}</h2>
-                  <p id="text" class="transition-all	 duration-200 line-clamp-3">${item.text}</p>
-                  <button id="button" class="text-blue-500 hover:text-blue-600">Show more</button>
+                  <p id={item.date} class="transition-all	 duration-200 line-clamp-3">${item.text}</p>
+                  <button id="${item.date}Button" class="text-blue-500 hover:text-blue-600">Show more</button>
                 </li>
             `;
             dataList.appendChild(listItem);
-              const text = document.getElementById("text");
-              const toggleButton = document.getElementById("button");
+              const text = document.getElementById(item.date);
+              const toggleButton = document.getElementById(item.date + "Button");
             
               toggleButton.addEventListener("click", function() {
                 text.classList.toggle("line-clamp-3");
@@ -29,8 +29,8 @@ async function fetchData() {
               });            
             
               if (text) {
-                const lineHeight = parseInt(getComputedStyle(text).lineHeight);
-                const maxHeight = lineHeight * 3;
+                // const lineHeight = parseInt(getComputedStyle(text).lineHeight);
+                const maxHeight = 24 * 3;
                 const actualHeight = text.offsetHeight;
                 if (actualHeight < maxHeight) {
                   toggleButton.classList.add("hidden");
