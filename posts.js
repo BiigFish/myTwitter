9573674f3ea908dc8da0fc4290157fe8397e7274
Evan <evchan29@gmail.com>
2023-10-08 17:06:26 -0400

    div fix

--- posts.js
+++ posts.js
@@ -11,7 +11,8 @@ async function fetchData() {
                 <li id="${item.date}" class="bg-gray-200 rounded-lg p-2">
                   <div class="flex justify-between">
                     <h2 class="font-bold text-sm text-red-400 w-fit">${item.title}</h2>
-                    <button onClick="copyFunction('https://x.ratnotebook.com#${item.date}')" class="bg-slate-100 rounded-full px-1 text-sm text-slate-600">copy text</button>
+                    <button onClick="copyFunction('https://x.ratnotebook.com#${item.date}')" class="bg-slate-100 rounded-full px-1 text-sm text-slate-600">share post</button>
+                  </div>
                   <p class="text-xs italic text-gray-600">${item.date}</p>
                   <div id="${item.date}" class="transition-all duration-200 line-clamp-3">${item.text}</div>
                   <button id="${item.date}Button" class="text-blue-500 hover:text-blue-600">Show more</button>
